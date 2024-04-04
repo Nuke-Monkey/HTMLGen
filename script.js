6dc12113134319213dc2eba2c0e022b26d01109c
Nuke-Monkey <126367570+Nuke-Monkey@users.noreply.github.com>
2024-04-03 20:06:25 -0400

    Adds set_text() to allow for adding text to elements and changes $ prefix to & to fix any issues beforehand

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ let link_amount = 0
 
 
 var elem = {
-    "paragraph": "<p id= $#>$T</p>",
-    "button": "<button id= $#>$T</button>",
-    "link": "<a href=$A id= $#>$T</a>"
+    "paragraph": "<p id= &#>&T</p>",
+    "button": "<button id= &#>&T</button>",
+    "link": "<a href=&A id= &#>&T</a>"
 }
 
 function set_id(element_type, element, element_amount) {
-    return element.replace("$#", element_type + element_amount)
+    return element.replace("&#", element_type + element_amount)
 }
 
+function set_text(element, text) {
+    return element.replace("&T", text)
+}
+
+//Sets up element ids
 console.log(set_id("paragraph", elem.paragraph, paragraph_amount))
 console.log(set_id("button", elem.button, button_amount))
 console.log(set_id("link", elem.link, link_amount))
+
+//Adds text to the elements
+console.log(set_text(elem.paragraph, "Hello, World!"))
+console.log(set_text(elem.button, "Click Me!"))
+console.log(set_text(elem.link, "Example.com"))
